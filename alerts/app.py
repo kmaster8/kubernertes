c5ce9a56317c3a9fad4fb86ee4acd70580a3b644
kmaster <kaster8k@gmail.com>
2022-02-27 21:19:38 +0900

    append alerts

---
+++ alerts/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/robusta-alerts",methods=['POST'])
+def robusta-alerts():
+  if request.method == 'POST':
+    print('Webhook Received')
+    request_json = request.json
+
+    print('Payload: ')
+    print(json.dumps(request_json,indent=4))
+    return 'Webhook notification received', 202
+  else:
+    return 'POST Method not supported', 405
+    
+
+if __name__ == '__main__':
+    app.run()
